c53d07b14bda9e80d45b6ad6a23f697d2255a4bf
Long-Soeng167 <generalpurpose167@gmail.com>
2025-05-31 11:59:31 +0700

    courses

--- app/Http/Controllers/Api/CourseController.php
+++ app/Http/Controllers/Api/CourseController.php
@@ -13,17 +13,51 @@ class CourseController extends Controller
     public function index(Request $request)
     {
         $search = $request->search;
+
         if ($search) {
             $items = Course::where('title', 'LIKE', "%$search%")->paginate(10);
         } else {
             $items = Course::paginate(10);
         }
+
+        // Transform each item in the paginated data
+        $items->getCollection()->transform(function ($item) {
+            return [
+                'id' => $item->id,
+                'title' => $item->title,
+                'title_kh' => $item->title_kh,
+                'image' => $item->image,
+                'description' => $item->short_description, // or null if you want
+                'description_kh' => $item->short_description_kh, // or null if you want
+                'price' => $item->price,
+                'start' => $item->start_at,
+                'end' => $item->end_at,
+                'created_at' => $item->created_at,
+                'updated_at' => $item->updated_at,
+            ];
+        });
+
         return response()->json($items);
     }
+
 
     public function show(string $id)
     {
         $item = Course::findOrFail($id); // Retrieve the item with the given ID
-        return response()->json($item);
+        $data = [
+            'id' => $item->id,
+            'title' => $item->title,
+            'title_kh' => $item->title_kh,
+            'image' => $item->image,
+            'description' => $item->short_description, // or null if you want
+            'description_kh' => $item->short_description_kh, // or null if you want
+            'price' => $item->price,
+            'start' => $item->start_at,
+            'end' => $item->end_at,
+            'created_at' => $item->created_at,
+            'updated_at' => $item->updated_at,
+        ];
+
+        return response()->json($data);
     }
 }
