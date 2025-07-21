84a7bc4fb8d7c38b96494545e34c80db706551b7
Long-Soeng167 <generalpurpose167@gmail.com>
2025-07-21 10:43:58 +0700

    update no ssr

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -4,8 +4,7 @@ import '../css/ckeditor-custom.css';
 
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-// import { createRoot } from 'react-dom/client';
-import { hydrateRoot } from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import { initializeTheme } from './hooks/use-appearance';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
@@ -13,13 +12,10 @@ const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob('./pages/**/*.tsx')),
-    // setup({ el, App, props }) {
-    //     const root = createRoot(el);
-
-    //     root.render(<App {...props} />);
-    // },
     setup({ el, App, props }) {
-        hydrateRoot(el, <App {...props} />);
+        const root = createRoot(el);
+
+        root.render(<App {...props} />);
     },
     progress: {
         color: '#ff6467',
