36c9acac3abf9f4c1978879a6cac6d4894846eb5
Long-Soeng167 <generalpurpose167@gmail.com>
2025-09-27 19:13:34 +0700

    dd

--- app/Http/Controllers/Api/FileExploreController.php
+++ app/Http/Controllers/Api/FileExploreController.php
@@ -32,7 +32,7 @@ class FileExploreController extends Controller
 
         return response()->json([
             'user' => $user ? $user : null,
-            'status' => 'need_login', //can_read, need_login, need_purchase,
+            'status' => 'need_purchase', //can_read, need_login, need_purchase,
             'files' => $files,
             'folders' => $folders,
             'path' => $path
