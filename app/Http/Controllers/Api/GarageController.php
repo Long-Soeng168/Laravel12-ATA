5aa9094f7f9477bbe0cb0595fc155b9110b1ddb2
Long-Soeng167 <generalpurpose167@gmail.com>
2026-01-30 14:05:51 +0700

    update

--- app/Http/Controllers/Api/GarageController.php
+++ app/Http/Controllers/Api/GarageController.php
@@ -52,7 +52,7 @@ class GarageController extends Controller
         $query->orderBy('order_index');
         $query->orderBy('id', 'desc');
 
-        $garages = $query->paginate(100);
+        $garages = $query->paginate(1000);
 
         // Map to old key format
         $convertedGarages = $garages->getCollection()->map(function ($garage) {
