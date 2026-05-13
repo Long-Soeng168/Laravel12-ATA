9b19eb931359b75dd35dab88a8dd37e5504c6e4e
Long-Soeng167 <generalpurpose167@gmail.com>
2026-05-13 18:40:58 +0700

    update

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -161,8 +161,6 @@ class VideoController extends Controller implements HasMiddleware
             'is_free' => 'nullable|boolean',
             'title' => 'required|string|max:255',
             'title_kh' => 'nullable|string|max:255',
-            'video_file' => 'nullable|file|mimes:mp4|max:1258291',
-            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif,webp|max:2048',
             'playlist_code' => 'nullable|string|max:255',
             'order_index' => 'required|numeric|min:1|max:255',
             'status' => 'nullable|string|in:active,inactive',
