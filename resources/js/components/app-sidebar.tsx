5f55990d6856bb0d4c4e5e3711f72b1533a46929
Long-Soeng167 <generalpurpose167@gmail.com>
2026-06-30 18:24:10 +0700

    improve app sidebar

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -463,7 +463,7 @@ export function AppSidebar() {
             </SidebarContent>
 
             <SidebarFooter>
-                <NavFooter items={footerNavItems} className="mt-auto" />
+                {/* <NavFooter items={footerNavItems} className="mt-auto" /> */}
                 <NavUser />
             </SidebarFooter>
         </Sidebar>
